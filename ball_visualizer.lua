1367576a922f163c083668c16c6ccc4286de5914
Kiaran Lau <roboperson10@gmail.com>
2025-09-20 03:03:58 -0700

    ball visualizer, added remoteEvent and highlighting for 1 chunk

--- ball_visualizer.lua
+++ ball_visualizer.lua
@@ -1,6 +1,12 @@
 local region = workspace:FindFirstChild('PelletRegion') :: Part
 local chunkSize = Vector3.new(100, 10, 100)
 local pelletSpacing = 10
+local chunkTable = {}
+local numChunks = 0
+
+-- TODO: Remove, testing for player-chunk bounding check
+local spotlightChunk = nil -- Used to highlight a specific chunk for 1 player
+local PingChunkRemote = script:WaitForChild('PingChunk') :: RemoteEvent
 
 function createPellet(region, px, pz)
 	local pellet = Instance.new('Part')
@@ -24,6 +30,8 @@ function createPellet(region, px, pz)
 end
 
 function createChunk(region, px, pz)
+	numChunks += 1
+	
 	local chunk = Instance.new('Part')
 	chunk.Name = "Chunk"
 	chunk.Size = chunkSize
@@ -33,20 +41,21 @@ function createChunk(region, px, pz)
 	chunk.Color = Color3.new(1, 0, 0)
 	chunk.Position = Vector3.new(px, region.Position.Y + chunkSize.Y/2, pz)
 	chunk.Parent = workspace
-	return chunk
+	print(string.format("Chunk %d position: (%d, %d)", numChunks, chunk.Position.X, chunk.Position.Z))
+	local chunkHash = string.format("%d%d", chunk.Position.X, chunk.Position.Z)
+	chunkTable[chunkHash] = {chunk=chunk}
+	print(string.format("%s = {%s}", chunkHash, chunkTable[chunkHash].chunk.Name))
+	return chunk, chunkHash
 end
-
-chunkTable = {}
 
 function spawnPelletsForChunk(chunk : Part)
 	local cx = chunkSize.X
 	local cz = chunkSize.Z
-	for x = 0, cx-1, pelletSpacing do -- chunk size X - 1, prevents inclusion of ending
-		for z = 0, cz-1, pelletSpacing do
+	for x = 5, cx-1, pelletSpacing do -- chunk size X - 1, prevents inclusion of ending
+		for z = 5, cz-1, pelletSpacing do
 			local coordx = chunk.Position.X - cx/2 + x
 			local coordz = chunk.Position.Z - cz/2 + z
 			local pellet = createPellet(region, coordx, coordz)
-			wait()
 		end
 	end
 end
@@ -56,12 +65,27 @@ function spawnChunksForRegion(region : Part)
 		for z = 0, region.Size.Z-1, chunkSize.Z do
 			local coordx = x + chunkSize.X/2 - region.Size.X/2
 			local coordz = z + chunkSize.Z/2 - region.Size.Z/2
-			local chunk = createChunk(region, coordx, coordz)
+			local chunk, chunkHash = createChunk(region, coordx, coordz)
 			spawnPelletsForChunk(chunk)
-			table.insert(chunkTable, chunk)
-			print( string.format("Created chunk at (%d, %d) -> x, z = (%d, %d)", coordx, coordz, x, z) )
+			--print( string.format("Created chunk at (%d, %d) -> x, z = (%d, %d)", coordx, coordz, x, z) )
 		end
 	end
 end
 
 spawnChunksForRegion(region)
+
+-- Intitialize spotlightChunk
+spotlightChunk = chunkTable["5050"].chunk
+
+
+PingChunkRemote.OnServerEvent:Connect(function(player: Player, hash)
+	--local hash = string.format("%d%d", posx, posz)
+	if chunkTable[hash] and chunkTable[hash].chunk ~= spotlightChunk then
+		-- Reset previous spotlightChunk
+		spotlightChunk.Color = Color3.new(1, 0, 0)
+		
+		-- Highlight new chunk
+		spotlightChunk = chunkTable[hash].chunk
+		spotlightChunk.Color = Color3.new(0, 1, 0)
+	end
+end)
